b546e71a426dbdaa77b199227a27cba66a7f4338
daqingdie <532131417@qq.com>
2019-09-06 13:30:02 +0800

    实验3

--- src/main/java/com/lq/service/impl/CountServiceImpl.java
+++ src/main/java/com/lq/service/impl/CountServiceImpl.java
@@ -8,23 +8,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class CountServiceImpl implements CountService {
-   /* public CountServiceImpl(){
-        System.out.println("make");
-    }*/
 
 
 
 
     public void save() {
         System.out.println("haha");
-        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
-        CountDao countDao = (CountDao) ac.getBean("countDao");
-
-        countDao.saveAll();
 
     }
 
-    public CountDaoImpl getCountServiceImpl(){
-        return  new CountDaoImpl();
-    }
 }
--- src/test/java/com/lq/test.java
+++ src/test/java/com/lq/test.java
@@ -15,30 +15,10 @@ import java.applet.AppletContext;
 
 
 public class test {
-    @Test
-    public void test(){
-//        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
-//        CountService countService = (CountService)ac.getBean("countService");
-//        countService.save();
-//        CountDao countDao = (CountDao) ac.getBean("countDao");
-//        countDao.saveAll();
-//        CountService countService1=ac.getBean("countService",CountService.class);
-//        countService1.save();
-        Resource resource = new ClassPathResource("bean.xml");
-        BeanFactory beanFactory =new  XmlBeanFactory(resource);
-        CountService countService = (CountService) beanFactory.getBean("countService");
-        countService.save();
-        CountDao countDao = (CountDao) beanFactory.getBean("countDao2");
-        countDao.saveAll();
-//        countDao.saveAll();
-    }
 
     @Test
     public void test_1(){
-        ClassPathXmlApplicationContext cpac=new ClassPathXmlApplicationContext("bean.xml");
-        CountDao countDao=(CountDao)cpac.getBean("countDao");
-        countDao.saveAll();
-        cpac.close();
+
     }
 
 
